dd4dbb9bfe10ec73fe66e7a53e9c228a1b7fadec
Dennis Grebe <dgrebe@uni-bremen.de>
2015-11-23 00:33:26 +0100

    Initial, very early, very incomplete version

---
+++ rF2ResultsToCsv/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace rF2ResultsToCsv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveFileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Drivers";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openFileToolStripMenuItem,
+            this.saveFileToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openFileToolStripMenuItem
+            // 
+            this.openFileToolStripMenuItem.Name = "openFileToolStripMenuItem";
+            this.openFileToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.openFileToolStripMenuItem.Text = "Open File";
+            this.openFileToolStripMenuItem.Click += new System.EventHandler(this.openFileToolStripMenuItem_Click);
+            // 
+            // saveFileToolStripMenuItem
+            // 
+            this.saveFileToolStripMenuItem.Name = "saveFileToolStripMenuItem";
+            this.saveFileToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.saveFileToolStripMenuItem.Text = "Save File";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(0, 27);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Load Results File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openFileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveFileToolStripMenuItem;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ rF2ResultsToCsv/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+
+namespace rF2ResultsToCsv
+{
+    public partial class Form1 : Form
+    {
+        string GetFilePath ()
+        {
+            string fp_ResultsFile;
+            OpenFileDialog openFileDialog_ResultsFiles = new OpenFileDialog();
+            // Set filter options and filter index.
+            openFileDialog_ResultsFiles.Filter = "rF2 results file (.xml)|*.xml|All Files (*.*)|*.*";
+            openFileDialog_ResultsFiles.FilterIndex = 1;
+            openFileDialog_ResultsFiles.Multiselect = false;
+
+            if (openFileDialog_ResultsFiles.ShowDialog() == DialogResult.OK)
+            {
+                fp_ResultsFile = openFileDialog_ResultsFiles.FileName;
+            }
+            else
+            {
+                fp_ResultsFile = "Error";
+            }
+            MessageBox.Show(fp_ResultsFile);
+            return fp_ResultsFile;
+        }
+  
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            XmlDocument xmlinput = new XmlDocument();
+            string filepath = GetFilePath();// @"G:\rFactor2_VEC\UserData\Log\Results\2015_10_31_19_14_20-74Q1.xml";
+            xmlinput.Load(filepath);
+            if (xmlinput.HasChildNodes)
+            {
+                MessageBox.Show("xmpinput ist nicht leer");
+            }
+            XmlNodeList xmlnode = xmlinput.DocumentElement.SelectNodes("//Driver");
+            if (xmlnode.Count > 0)
+            {
+                MessageBox.Show("xmlnode ist nicht leer: " + xmlnode.Count);
+            }
+            string driver_name = "";
+            foreach (XmlNode node in xmlnode)
+            {
+                driver_name = node.SelectSingleNode("Name").InnerText;
+                MessageBox.Show(driver_name);
+            }
+        }
+
+        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            string fp_resultsfile;
+            OpenFileDialog openFileDialog_ResultsFiles = new OpenFileDialog();
+            // Set filter options and filter index.
+            openFileDialog_ResultsFiles.Filter = "rF2 results file (.xml)|*.xml|All Files (*.*)|*.*";
+            openFileDialog_ResultsFiles.FilterIndex = 1;
+            openFileDialog_ResultsFiles.Multiselect = false;
+
+            // Call the ShowDialog method to show the dialog box.
+            if (openFileDialog_ResultsFiles.ShowDialog() == DialogResult.OK)
+            {
+                fp_resultsfile = openFileDialog_ResultsFiles.FileName;
+            }
+            else
+            {
+                fp_resultsfile = "null";
+            }
+/* return fp_resultsfile; */
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string fp_ResultsFile;
+
+            OpenFileDialog openFileDialog_ResultsFiles = new OpenFileDialog();
+            // Set filter options and filter index.
+            openFileDialog_ResultsFiles.Filter = "rF2 results file (.xml)|*.xml|All Files (*.*)|*.*";
+            openFileDialog_ResultsFiles.FilterIndex = 1;
+            openFileDialog_ResultsFiles.Multiselect = false;
+
+            if (openFileDialog_ResultsFiles.ShowDialog() == DialogResult.OK)
+            {
+                fp_ResultsFile = openFileDialog_ResultsFiles.FileName;
+            }
+            else
+            {
+                fp_ResultsFile = "Error";
+
+            }
+
+            MessageBox.Show(fp_ResultsFile);
+
+        }
+    }
+}
