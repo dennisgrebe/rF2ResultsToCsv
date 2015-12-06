4f3956bfbd0a27529566a216cec87b07b4bd85df
Dennis Grebe <dgrebe@uni-bremen.de>
2015-12-06 21:28:30 +0100

    Added first selection attempts of lap info, trial output for data

--- rF2ResultsToCsv/Form1.cs
+++ rF2ResultsToCsv/Form1.cs
@@ -14,6 +14,8 @@ namespace rF2ResultsToCsv
 {
     public partial class Form1 : Form
     {
+
+        // Function to select a file and return the filepath
         string GetFilePath ()
         {
             string fp_ResultsFile;
@@ -34,7 +36,24 @@ namespace rF2ResultsToCsv
             MessageBox.Show(fp_ResultsFile);
             return fp_ResultsFile;
         }
-  
+        // Define class for holding the relevant lap data
+        public class rf2_lap
+        {
+            public string carclass { get; set; }
+            public string carnumber { get; set; }
+            public string teamname { get; set; }
+            public string lap_num { get; set; }
+            public string lap_pos { get; set; }
+            public string lap_start { get; set; }
+            public string s1 { get; set; }
+            public string s2 { get; set; }
+            public string s3 { get; set; }
+            public string fuel { get; set; }
+            public string tires_front { get; set; }
+            public string tires_rear { get; set; }
+            public string pit { get; set; }
+            public string laptime { get; set; }
+        }
 
         public Form1()
         {
@@ -43,7 +62,7 @@ namespace rF2ResultsToCsv
         private void Form1_Load(object sender, EventArgs e)
         {
         }
-
+        // Currently named "Drivers"
         private void button1_Click(object sender, EventArgs e)
         {
             XmlDocument xmlinput = new XmlDocument();
@@ -51,19 +70,73 @@ namespace rF2ResultsToCsv
             xmlinput.Load(filepath);
             if (xmlinput.HasChildNodes)
             {
-                MessageBox.Show("xmpinput ist nicht leer");
+                MessageBox.Show("Selected resultsfile is not empty and has been loaded.");
             }
             XmlNodeList xmlnode = xmlinput.DocumentElement.SelectNodes("//Driver");
             if (xmlnode.Count > 0)
             {
-                MessageBox.Show("xmlnode ist nicht leer: " + xmlnode.Count);
+                MessageBox.Show("Drivers list is not empty. " + xmlnode.Count + " drivers have been found in the results file.");
             }
-            string driver_name = "";
+            // Create list to dump lap data into
+            List<rf2_lap> laps_list = new List<rf2_lap>();
+
+
+
             foreach (XmlNode node in xmlnode)
             {
+                // Find and set driver name
+                string driver_name = "";
                 driver_name = node.SelectSingleNode("Name").InnerText;
-                MessageBox.Show(driver_name);
+
+
+                // Select all laps for a given driver.
+                XmlNodeList laps = xmlinput.DocumentElement.SelectNodes("//Driver[Name=\"" + driver_name + "\"]/Lap");
+
+                // Debugging outputs
+                if (laps.Count > 0)
+                {
+                    MessageBox.Show("Number of laps found for driver " + driver_name + ": " + laps.Count);
+                }
+                // Add laps to list
+                if (driver_name == "Dennis Grebe")
+                {
+
+                    foreach (XmlNode lapnode in laps)
+                    {
+                        // If all sectors exist in results file load complete lap data - currently restricted to laps which have s1 to s3 defined as those elements are missing entirely if not recorded
+                        if (lapnode.Attributes["s1"] != null && lapnode.Attributes["s2"] != null && lapnode.Attributes["s3"] != null)
+                        {
+                            laps_list.Add(new rf2_lap
+                            {
+                                carclass = node.SelectSingleNode("CarClass").InnerText ?? "N/A",
+                                carnumber = node.SelectSingleNode("CarNumber").InnerText ?? "N/A",
+                                teamname = node.SelectSingleNode("TeamName").InnerText ?? "N/A",
+                                lap_num = lapnode.Attributes["num"].Value ?? "N/A",
+                                lap_pos = lapnode.Attributes["p"].Value ?? "N/A",
+                                lap_start = lapnode.Attributes["et"].Value ?? "N/A",
+                                s1 = lapnode.Attributes["s1"].Value ?? "N/A",
+                                s2 = lapnode.Attributes["s2"].Value ?? "N/A",
+                                s3 = lapnode.Attributes["s3"].Value ?? "N/A",
+                                fuel = lapnode.Attributes["fuel"].Value ?? "N/A",
+                                tires_front = lapnode.Attributes["fcompound"].Value ?? "N/A",
+                                tires_rear = lapnode.Attributes["rcompound"].Value ?? "N/A",
+                                pit = "1", //lapnode.Attributes["pit"].Value,
+                                laptime = lapnode.InnerText ?? "N/A",
+                            }
+                            );
+                        }
+                    }                    
+
+                    rf2_lap output = laps_list[1];
+                    MessageBox.Show("Carclass: " + output.carclass + Environment.NewLine + "CarNumber: " + output.carnumber + Environment.NewLine + "TeamName: " + output.teamname + Environment.NewLine + "Lap number: " + output.lap_num + Environment.NewLine
+                        + "Lap Pos: " + output.lap_pos + Environment.NewLine + "Lap start: " + output.lap_start + Environment.NewLine + "Sector 1: " + output.s1 + Environment.NewLine
+                        + "Sector 2: " + output.s2 + Environment.NewLine + "Sector 3: " + output.s3 + Environment.NewLine + "Fuel: " + output.fuel + Environment.NewLine
+                        + "Tires Front: " + output.tires_front + Environment.NewLine + "Tires Rear: " + output.tires_rear + Environment.NewLine + "Laptime : " + output.laptime);
+                    
+                }
+                //MessageBox.Show(driver_name);
             }
+           // string[,] array = new string[,];
         }
 
         private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
@@ -92,6 +165,7 @@ namespace rF2ResultsToCsv
 /* return fp_resultsfile; */
         }
 
+        // Load results file button at the top
         private void button2_Click(object sender, EventArgs e)
         {
             string fp_ResultsFile;
