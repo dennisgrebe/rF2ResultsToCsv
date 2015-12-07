eb33cf9835e7eda9688202dd53d27827a01e535f
Dennis Grebe <dgrebe@uni-bremen.de>
2015-12-07 22:43:10 +0100

    First version of csv export included, now writes all valid laps to list

--- rF2ResultsToCsv/Form1.cs
+++ rF2ResultsToCsv/Form1.cs
@@ -36,9 +36,32 @@ namespace rF2ResultsToCsv
             MessageBox.Show(fp_ResultsFile);
             return fp_ResultsFile;
         }
+        string SaveFilePath()
+        {
+            string fp_SaveFile;
+            SaveFileDialog saveFileDialog_SaveFile = new SaveFileDialog();
+            // Set filter options and filter index.
+            saveFileDialog_SaveFile.Filter = "Comma-separated file (.csv)|*.csv|All Files (*.*)|*.*";
+            saveFileDialog_SaveFile.Title = "Save results to .csv-file:";
+            if (saveFileDialog_SaveFile.ShowDialog() == DialogResult.OK)
+            {
+                fp_SaveFile = saveFileDialog_SaveFile.FileName;
+            }
+            else
+            {
+                fp_SaveFile = "Error";
+            }
+            MessageBox.Show(fp_SaveFile);
+            return fp_SaveFile;
+        }
+
+
+
+
         // Define class for holding the relevant lap data
         public class rf2_lap
         {
+            public string driver { get; set; }
             public string carclass { get; set; }
             public string carnumber { get; set; }
             public string teamname { get; set; }
@@ -62,7 +85,7 @@ namespace rF2ResultsToCsv
         private void Form1_Load(object sender, EventArgs e)
         {
         }
-        // Currently named "Drivers"
+        // Currently named "Parse"
         private void button1_Click(object sender, EventArgs e)
         {
             XmlDocument xmlinput = new XmlDocument();
@@ -79,8 +102,9 @@ namespace rF2ResultsToCsv
             }
             // Create list to dump lap data into
             List<rf2_lap> laps_list = new List<rf2_lap>();
-
-
+            // Container for csv output
+            var csv = new StringBuilder();
+            string newline;
 
             foreach (XmlNode node in xmlnode)
             {
@@ -88,26 +112,25 @@ namespace rF2ResultsToCsv
                 string driver_name = "";
                 driver_name = node.SelectSingleNode("Name").InnerText;
 
-
                 // Select all laps for a given driver.
                 XmlNodeList laps = xmlinput.DocumentElement.SelectNodes("//Driver[Name=\"" + driver_name + "\"]/Lap");
 
                 // Debugging outputs
-                if (laps.Count > 0)
+                if (laps.Count < 0)
                 {
                     MessageBox.Show("Number of laps found for driver " + driver_name + ": " + laps.Count);
                 }
                 // Add laps to list
-                if (driver_name == "Dennis Grebe")
-                {
 
                     foreach (XmlNode lapnode in laps)
                     {
-                        // If all sectors exist in results file load complete lap data - currently restricted to laps which have s1 to s3 defined as those elements are missing entirely if not recorded
+                        // If all sectors exist in results file load complete lap data - currently restricted to laps which have s1 to s3 defined as those elements are missing entirely if not recorded. 
+                        // Should refactor this bit to first load attribute into a variable and set it to a default if we're not able to get data from the results file
                         if (lapnode.Attributes["s1"] != null && lapnode.Attributes["s2"] != null && lapnode.Attributes["s3"] != null)
                         {
-                            laps_list.Add(new rf2_lap
-                            {
+                        laps_list.Add(new rf2_lap
+                        {
+                                driver = driver_name,
                                 carclass = node.SelectSingleNode("CarClass").InnerText ?? "N/A",
                                 carnumber = node.SelectSingleNode("CarNumber").InnerText ?? "N/A",
                                 teamname = node.SelectSingleNode("TeamName").InnerText ?? "N/A",
@@ -124,70 +147,23 @@ namespace rF2ResultsToCsv
                                 laptime = lapnode.InnerText ?? "N/A",
                             }
                             );
-                        }
-                    }                    
-
-                    rf2_lap output = laps_list[1];
-                    MessageBox.Show("Carclass: " + output.carclass + Environment.NewLine + "CarNumber: " + output.carnumber + Environment.NewLine + "TeamName: " + output.teamname + Environment.NewLine + "Lap number: " + output.lap_num + Environment.NewLine
-                        + "Lap Pos: " + output.lap_pos + Environment.NewLine + "Lap start: " + output.lap_start + Environment.NewLine + "Sector 1: " + output.s1 + Environment.NewLine
-                        + "Sector 2: " + output.s2 + Environment.NewLine + "Sector 3: " + output.s3 + Environment.NewLine + "Fuel: " + output.fuel + Environment.NewLine
-                        + "Tires Front: " + output.tires_front + Environment.NewLine + "Tires Rear: " + output.tires_rear + Environment.NewLine + "Laptime : " + output.laptime);
-                    
+                        }          
                 }
-                //MessageBox.Show(driver_name);
+                // Stuff to do for each driver goes here
             }
-           // string[,] array = new string[,];
-        }
-
-        private void openFileToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
-        {
-            string fp_resultsfile;
-            OpenFileDialog openFileDialog_ResultsFiles = new OpenFileDialog();
-            // Set filter options and filter index.
-            openFileDialog_ResultsFiles.Filter = "rF2 results file (.xml)|*.xml|All Files (*.*)|*.*";
-            openFileDialog_ResultsFiles.FilterIndex = 1;
-            openFileDialog_ResultsFiles.Multiselect = false;
-
-            // Call the ShowDialog method to show the dialog box.
-            if (openFileDialog_ResultsFiles.ShowDialog() == DialogResult.OK)
+            // Stuff done once after cycling through all the laps and drivers goes here
+            int num_laps; int n;
+            num_laps = laps_list.Count;
+            n = 0;
+            while (n < num_laps)
             {
-                fp_resultsfile = openFileDialog_ResultsFiles.FileName;
+                rf2_lap output = laps_list[n];
+                newline = string.Format("{0};{1};{2};{3};{4};{5};{6};{7};{8};{9};{10};{11};{12};{13};{14}", output.driver, output.carclass, output.carnumber, output.teamname, output.lap_num, output.lap_pos, output.lap_start, output.s1, output.s2, output.s3, output.fuel, output.tires_front, output.tires_rear, output.pit, output.laptime);
+                csv.AppendLine(newline);
+                n++;
             }
-            else
-            {
-                fp_resultsfile = "null";
-            }
-/* return fp_resultsfile; */
-        }
-
-        // Load results file button at the top
-        private void button2_Click(object sender, EventArgs e)
-        {
-            string fp_ResultsFile;
-
-            OpenFileDialog openFileDialog_ResultsFiles = new OpenFileDialog();
-            // Set filter options and filter index.
-            openFileDialog_ResultsFiles.Filter = "rF2 results file (.xml)|*.xml|All Files (*.*)|*.*";
-            openFileDialog_ResultsFiles.FilterIndex = 1;
-            openFileDialog_ResultsFiles.Multiselect = false;
-
-            if (openFileDialog_ResultsFiles.ShowDialog() == DialogResult.OK)
-            {
-                fp_ResultsFile = openFileDialog_ResultsFiles.FileName;
-            }
-            else
-            {
-                fp_ResultsFile = "Error";
-
-            }
-
-            MessageBox.Show(fp_ResultsFile);
-
+            string output_fpath = SaveFilePath();
+            File.WriteAllText(output_fpath, csv.ToString());
         }
     }
 }
